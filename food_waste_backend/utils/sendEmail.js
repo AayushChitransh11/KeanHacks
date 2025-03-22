c3820d9f6a10d30568a3398e7f18f25b30806242
AayushChitransh11 <aayush11.ac@gmail.com>
2025-03-22 18:39:32 -0400

    edited api

--- food_waste_backend/utils/sendEmail.js
+++ food_waste_backend/utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Food Waste App <onboarding@thefoodshare.dev>', // you can use this in dev mode
+      from: 'Food Waste App <onboarding@resend.dev>', // you can use this in dev mode
       to,
       subject,
       html
